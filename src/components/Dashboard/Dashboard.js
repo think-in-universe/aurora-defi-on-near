68f53c7085e5a76073c4fcdba6254e8f062a1283
think-in-universe <46699230+think-in-universe@users.noreply.github.com>
2022-03-10 08:33:46 +0800

    feat: load Aurora ERC20 token address from NEP141 account ID

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,6 +8,9 @@ import "./Dashboard.scss";
 import * as nearAPI from "near-api-js";
 import { Erc20Abi } from "../../abi/erc20";
 import { useAccount } from "../../data/account";
+import { AccountID, Address } from "@aurora-is-near/engine";
+
+const wNEAR = NearConfig.wrapNearAccountId;
 
 const fetchBalance = async (aurora, address) => {
   return Big((await aurora.getBalance(toAddress(address))).unwrap());
@@ -41,12 +44,21 @@ export default function Dashboard(props) {
     : [];
   sortedErc20Balances.sort(([t1, a], [t2, b]) => b.cmp(a));
 
-  const depositWNear = async (e, amount) => {
+
+  const getErc20Addr = async (nep141) => {
+    return (await aurora.getAuroraErc20Address(new AccountID(nep141))).unwrap().toString();
+  }
+
+  const getNep141 = async (erc20) => {
+    return (await aurora.getNEP141Account(new Address(erc20))).unwrap().toString();
+  }
+
+  const depositToken = async (e, token, amount) => {
     e.preventDefault();
     setLoading(true);
     const actions = [
       [
-        NearConfig.wrapNearAccountId,
+        token,
         nearAPI.transactions.functionCall(
           "ft_transfer_call",
           {
@@ -64,17 +76,19 @@ export default function Dashboard(props) {
     await near.sendTransactions(actions);
   };
 
-  const withdrawWNear = async (e, amount) => {
+  const withdrawToken = async (e, token, amount) => {
     e.preventDefault();
     setLoading(true);
     const input = buildInput(Erc20Abi, "withdrawToNear", [
       `0x${Buffer.from(account.accountId, "utf-8").toString("hex")}`,
       OneNear.mul(amount).round(0, 0).toFixed(0),
     ]);
-    const wnearAddress = "0xC42C30aC6Cc15faC9bD938618BcaA1a1FaE8501d";
-    const res = (await aurora.call(toAddress(wnearAddress), input)).unwrap();
-    console.log(res);
-    setLoading(false);
+    const erc20Addr = await getErc20Addr(token);
+    if (erc20Addr) {
+      const res = (await aurora.call(toAddress(erc20Addr), input)).unwrap();
+      console.log(res);
+      setLoading(false);
+    }
   };
 
   return (
@@ -83,13 +97,13 @@ export default function Dashboard(props) {
       <div>
         <button
           className="btn btn-primary m-1"
-          onClick={(e) => depositWNear(e, 1)}
+          onClick={(e) => depositToken(e, wNEAR, 1)}
         >
           Deposit 1 wNEAR
         </button>
         <button
           className="btn btn-primary m-1"
-          onClick={(e) => withdrawWNear(e, 1)}
+          onClick={(e) => withdrawToken(e, wNEAR, 1)}
         >
           Withdraw 1 wNEAR
         </button>
