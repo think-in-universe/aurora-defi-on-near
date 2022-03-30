9d70bf8171ccbcd2e5af1b59c049685d927c89cf
think-in-universe <46699230+think-in-universe@users.noreply.github.com>
2022-03-30 19:10:17 +0800

    Updates

---
+++ precache-manifest.f6cd2c6623bcd6aad5dcb24f2cfa9046.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0aca28aa15ddd5096309404fae6a5ec",
+    "url": "/ref-aurora-frontend/index.html"
+  },
+  {
+    "revision": "d62016246a14f8152201",
+    "url": "/ref-aurora-frontend/static/css/2.27e2041f.chunk.css"
+  },
+  {
+    "revision": "6aebffa9e603409d93f7",
+    "url": "/ref-aurora-frontend/static/css/main.45a2763a.chunk.css"
+  },
+  {
+    "revision": "d62016246a14f8152201",
+    "url": "/ref-aurora-frontend/static/js/2.014f0b78.chunk.js"
+  },
+  {
+    "revision": "9bf9f295bc0647a7dc06c9db7621b73c",
+    "url": "/ref-aurora-frontend/static/js/2.014f0b78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aebffa9e603409d93f7",
+    "url": "/ref-aurora-frontend/static/js/main.a835e18f.chunk.js"
+  },
+  {
+    "revision": "390801274704c7e1e579",
+    "url": "/ref-aurora-frontend/static/js/runtime-main.464991e0.js"
+  },
+  {
+    "revision": "b0dc21936e27b98d6c65bd72dd705c65",
+    "url": "/ref-aurora-frontend/static/media/bootstrap-icons.b0dc2193.woff"
+  },
+  {
+    "revision": "c5787b4ab55835580294696c3cbcc521",
+    "url": "/ref-aurora-frontend/static/media/bootstrap-icons.c5787b4a.woff2"
+  }
+]);
